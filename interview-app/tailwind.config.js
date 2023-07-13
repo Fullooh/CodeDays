b0ad0de20dd61fdeed9e790616bd770d73c1ed83
Fullooh <angstdemon1@aol.com>
2023-07-12 21:21:27 -0400

    Update to repo

---
+++ interview-app/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{jsx,js,html}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['inter', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
+
