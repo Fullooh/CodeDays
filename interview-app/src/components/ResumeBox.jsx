3da3c444ab640863c66b3878ccf3514ea3e7cca0
Fullooh <angstdemon1@aol.com>
2023-07-26 17:36:44 -0400

    Update to repo

--- interview-app/src/components/ResumeBox.jsx
+++ interview-app/src/components/ResumeBox.jsx
@@ -36,6 +36,9 @@ function ResumeBox() {
     return (
         <div className="flex flex-col mb-10 mx-auto">
             <div className="flex justify-center items-center" style={{backgroundColor: "#EFEEEE", height: '75px'}}>Interview Simulator</div>
+            <label
+                className="p-2 text-black rounded-md cursor-pointer"
+            >
             <input 
                 type="file"
                 name="name"
@@ -43,24 +46,29 @@ function ResumeBox() {
                 className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
                 onChange={handleFileChange}
             />
+            </label>
+            <div className="flex justify-start">
             <button onClick={handleSubmit} 
-            className="inline-block px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 mx-auto"
-            >Upload Your Resume</button>
-
+            className="inline-block px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 ml-4"
+            >Upload Your Resume
+            </button>
+                </div>
             {/* Conditionally rendering loading text or server response */}
             {isLoading ? (
                 <div className="mt-5">
                     <p>Loading...</p>
                 </div>
             ) : responseData ? (
-                <div className="mt-5">
-                  <h3 className="text-xl">Interview Questions:</h3>
-                  {responseData.split('\n').map((response, index) => (
-                    <div key={index} className="border p-4 rounded-lg shadow-md mb-4">
-                      <p className="text-lg font-bold">Question {index + 1}</p>
-                      <p>{response}</p>
+                <div className="flex items-center">
+                    <div className="max-w-md mt-4 bg-gray-100 p-4 rounded-lg shadow-md h-80 overflow-y-auto">
+                    <h3 className="text-xl font-bold mb-4">Interview Questions:</h3>
+                        {responseData.split('\n').map((response, index) => (
+                        <div key={index} className="border p-2 rounded-lg shadow-md mb-2">
+                            <p className="text-lg font-bold">Question {index + 1}</p>
+                                <p>{response}</p>
+                        </div>
+    ))}
                     </div>
-                  ))}
                 </div>
               ) : null}
         </div>
